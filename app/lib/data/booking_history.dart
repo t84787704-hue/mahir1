b66e78531bc0af0af5ef1844ecbf998d415e2afe
t84787704-hue <t84787704@gmail.com>
2026-07-18 07:02:11 +0300

    Update booking_history.dart

---
+++ app/lib/data/booking_history.dart
@@ -0,0 +1,24 @@
+import '../models/worker_model.dart';
+
+class BookingHistory {
+  static final List<Map<String, dynamic>> bookings = [];
+
+  static void addBooking({
+    required WorkerModel worker,
+    required String date,
+    required String time,
+    required String address,
+    required String problem,
+    required String price,
+  }) {
+    bookings.insert(0, {
+      "worker": worker,
+      "date": date,
+      "time": time,
+      "address": address,
+      "problem": problem,
+      "price": price,
+      "status": "Pending",
+    });
+  }
+}
