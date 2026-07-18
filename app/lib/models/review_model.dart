62357bacdac4ce417070292583f06926fb368725
t84787704-hue <t84787704@gmail.com>
2026-07-18 16:09:23 +0300

    Create review_model.dart

---
+++ app/lib/models/review_model.dart
@@ -0,0 +1,11 @@
+class ReviewModel {
+  final String workerName;
+  final double rating;
+  final String review;
+
+  ReviewModel({
+    required this.workerName,
+    required this.rating,
+    required this.review,
+  });
+}
