1adbc02237b85f7d267d4b8d24a191cbf6a8ab5d
t84787704-hue <t84787704@gmail.com>
2026-07-18 06:31:32 +0300

    Create booking_model.dart

---
+++ app/lib/models/booking_model.dart
@@ -0,0 +1,21 @@
+class BookingModel {
+  final String workerName;
+  final String category;
+  final String date;
+  final String time;
+  final String address;
+  final String problem;
+  final String price;
+  final String status;
+
+  BookingModel({
+    required this.workerName,
+    required this.category,
+    required this.date,
+    required this.time,
+    required this.address,
+    required this.problem,
+    required this.price,
+    required this.status,
+  });
+}
