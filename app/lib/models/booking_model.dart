095d03ab17fb694f9df4ad23f819c6d340e4e974
t84787704-hue <t84787704@gmail.com>
2026-07-19 08:16:47 +0300

    Update booking_model.dart

--- app/lib/models/booking_model.dart
+++ app/lib/models/booking_model.dart
@@ -1,4 +1,6 @@
 class BookingModel {
+  final String id;
+  final String workerId;
   final String workerName;
   final String category;
   final String date;
@@ -9,6 +11,8 @@ class BookingModel {
   final String status;
 
   BookingModel({
+    required this.id,
+    required this.workerId,
     required this.workerName,
     required this.category,
     required this.date,
@@ -18,4 +22,34 @@ class BookingModel {
     required this.price,
     required this.status,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'worker_id': workerId,
+      'worker_name': workerName,
+      'category': category,
+      'date': date,
+      'time': time,
+      'address': address,
+      'problem': problem,
+      'price': price,
+      'status': status,
+    };
+  }
+
+  factory BookingModel.fromMap(Map<String, dynamic> map) {
+    return BookingModel(
+      id: map['id'] ?? '',
+      workerId: map['worker_id'] ?? '',
+      workerName: map['worker_name'] ?? '',
+      category: map['category'] ?? '',
+      date: map['date'] ?? '',
+      time: map['time'] ?? '',
+      address: map['address'] ?? '',
+      problem: map['problem'] ?? '',
+      price: map['price'] ?? '',
+      status: map['status'] ?? '',
+    );
+  }
 }
